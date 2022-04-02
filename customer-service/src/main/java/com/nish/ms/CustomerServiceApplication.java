778b8ba3354b4b10e6496f4a136adaa8dbf6689c
nnares <nishchay.naresh@gmail.com>
2022-04-02 18:44:36 +0530

    +u customer-service

--- customer-service/src/main/java/com/nish/ms/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/nish/ms/CustomerServiceApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
